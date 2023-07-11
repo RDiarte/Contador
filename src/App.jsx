16ee6a70a72129e7cf4f7382ff0c8f3f6df22ba0
RDiarte <ricardodiarte@gmail.com>
2023-07-11 20:13:43 -0300

    agregamos componente titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Titulo from "./components/Titulo";
 
 function App() {
   return (
-    <div>
-      <h1>Primer app con React</h1>
-    </div>
+    <>
+      <Titulo></Titulo>
+    </>
   );
 }
 
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,14 @@
+const Titulo = () => {
+  //aca vamos a agregar logica
+
+  return (
+    //aca tambien podemos escribir logica
+    <section>
+      {/* aqui va todo el maquetado */}
+      <h1>Proyecto contador en React</h1>
+      <h2>esto es un subtitulo</h2>
+    </section>
+  );
+};
+
+export default Titulo;
