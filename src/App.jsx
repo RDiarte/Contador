b8a64ae9be944adf182b9fc00bbc23ac281367f4
RDiarte <ricardodiarte@gmail.com>
2023-07-12 01:02:51 -0300

    agrego boton

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Container } from "react-bootstrap";
 import "./App.css";
 import Titulo from "./components/Titulo";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Contador from "./components/contador";
 
 function App() {
   return (
     <Container>
       <Titulo></Titulo>
+      <Contador></Contador>
     </Container>
   );
 }
